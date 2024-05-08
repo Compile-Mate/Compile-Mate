30073662eee16bf836838545d8fd5fe5d1b008a3
Compile-Mate <169175066+Compile-Mate@users.noreply.github.com>
2024-05-08 11:56:28 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,21 +3,41 @@
 # Print a message indicating that the script is starting
 echo "Starting build process..."
 
-# Install dependencies from requirements.txt using Python 3.9
+# Create a virtual environment
+echo "Creating virtual environment..."
+python -m venv myenv
+if [ $? -ne 0 ]; then
+    echo "Failed to create virtual environment. Exiting."
+    exit 1
+fi
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source myenv/bin/activate
+if [ $? -ne 0 ]; then
+    echo "Failed to activate virtual environment. Exiting."
+    exit 1
+fi
+
+# Install dependencies from requirements.txt
 echo "Installing dependencies..."
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
 if [ $? -ne 0 ]; then
     echo "Failed to install dependencies. Exiting."
     exit 1
 fi
 
-# Run collectstatic command using Python 3.9
+# Run collectstatic command
 echo "Collecting static files..."
-python3.9 manage.py collectstatic
+python manage.py collectstatic
 if [ $? -ne 0 ]; then
     echo "Failed to collect static files. Exiting."
     exit 1
 fi
+
+# Deactivate the virtual environment
+echo "Deactivating virtual environment..."
+deactivate
 
 # Print a message indicating that the script has finished successfully
 echo "Build process completed successfully."
