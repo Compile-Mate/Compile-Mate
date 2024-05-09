43c174318f465897819af88eb36dc45b0d13c1af
Compile-Mate <169175066+Compile-Mate@users.noreply.github.com>
2024-05-09 21:28:53 +0530

    Update settings.py

--- Compile-Mate/settings.py
+++ Compile-Mate/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 # Added Manually
 
 STATICFILES_DIRS = [
