b0b7832bcdb1ecf139945d6b0fb6753345880faf
Compile-Mate <169175066+Compile-Mate@users.noreply.github.com>
2024-05-09 21:29:48 +0530

    Update urls.py

--- Compile-Mate/urls.py
+++ Compile-Mate/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.site.site_header = "Compile-Mate Admin"
 admin.site.site_title = "Compile-Mate Admin Portal"
@@ -27,5 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Home.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns() 
